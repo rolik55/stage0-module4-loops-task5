32277ff9e89a23a69a169f6ee3ecff95cd477604
rolik55 <65602933+rolik55@users.noreply.github.com>
2022-06-27 16:51:00 +0300

    Task finished

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -3,5 +3,16 @@ package school.mjc.stage0.loops.task5;
 public class Cross {
     public void printCross(int sideLength) {
 
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+                if (i == sideLength / 2)
+                    System.out.print(8);
+                else if (j == sideLength / 2)
+                    System.out.print(8);
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        int count = height;
+        for(int i=0;i<height;i++){
+            for(int j=0;j<(height-count)/2;j++)
+                System.out.print(" ");
+            for (int j=0;j<count;j++){
+                System.out.print(8);
+            }
+            for(int j=0;j<(height-count)/2;j++)
+                System.out.print(" ");
+            if(i<height/2 && count > 2)
+                count-=2;
+            else if (i>=height/2 && count<height)
+                count+=2;
+            System.out.println();
+        }
     }
 }
